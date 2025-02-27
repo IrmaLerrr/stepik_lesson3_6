07167d1c0455936aabbfde81e80bc6e7063fd106
IrinaLazarenko <irma.lerrr.wine@gmail.com>
2025-02-27 09:22:39 +0300

    correct assert

--- test_items.py
+++ test_items.py
@@ -5,5 +5,5 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_find_card_button(browser):
     browser.get(link)
-    button = browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket")
+    button = browser.find_elements(By.CSS_SELECTOR, ".btn-add-to-basket")
     assert button is not None, "button is not found"
